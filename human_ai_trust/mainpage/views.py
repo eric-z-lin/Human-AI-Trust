4b60bff5be9031d1966d06aaff1140c1822837c8
Jack <ja_killian@yahoo.com>
2020-11-03 15:17:37 -0500

    if get request...

--- human_ai_trust/mainpage/views.py
+++ human_ai_trust/mainpage/views.py
@@ -12,61 +12,61 @@ from django.http import HttpResponse
 def index(request):
 	"""View function for home page of site."""
 
-	# if request.source == 'load_new_patient':
-	# Load session Id
-	experiment_id = request.session['experiment_id']
-	experiment = ModelExperiment.objects.get(id=experiment_id)
+	if request.method == 'GET':
+		# Load session Id
+		experiment_id = request.session['experiment_id']
+		experiment = ModelExperiment.objects.get(id=experiment_id)
 
-	ml_model = experiment.field_model_ml_model
+		ml_model = experiment.field_model_ml_model
 
-	# Get patient case
-	generated_patient = experiment.generate_patient()
-	case = experiment.generated_patient_to_case(generated_patient)
-	
-	arr = ml_model.model_prediction(case)
-	model_prediction = arr[0]
-	model_confidence = arr[1]
-	ground_truth = arr[2]
-
-
-	initUserResponse = {
-		"field_data_point_string": case,
-		"field_ml_accuracy":  json.dumps(ml_model.accuracy_field),
-		"field_ml_calibration": json.dumps(ml_model.calibration_field),
-		"field_ml_prediction": model_prediction,
-		"field_ml_confidence": model_confidence,
-		"field_instance_ground_truth": ground_truth,
-		"field_user_prediction": None,
-		"field_user_did_update": None,
-		"field_user_disagree_reason_choices": None,
-		"field_user_disagree_reason_freetext": None,
-	}
+		# Get patient case
+		generated_patient = experiment.generate_patient()
+		case = experiment.generated_patient_to_case(generated_patient)
+		
+		arr = ml_model.model_prediction(case)
+		model_prediction = arr[0]
+		model_confidence = arr[1]
+		ground_truth = arr[2]
 
 
-
-	new_user_response = ModelUserResponse(
-							field_data_point_string= initUserResponse["field_data_point_string"],
-							field_ml_accuracy = initUserResponse["field_ml_accuracy"],
-							field_ml_calibration = initUserResponse["field_ml_calibration"],
-							field_ml_prediction = initUserResponse["field_ml_prediction"],
-							field_instance_ground_truth = initUserResponse["field_instance_ground_truth"],
-							field_user_prediction = initUserResponse["field_user_prediction"],
-							field_user_did_update = initUserResponse["field_user_did_update"],
-							field_user_disagree_reason_choices = initUserResponse["field_user_disagree_reason_choices"],
-							field_user_disagree_reason_freetext = initUserResponse["field_user_disagree_reason_freetext"],
-							field_experiment = experiment
-						)
-
-	new_user_response.save()
+		initUserResponse = {
+			"field_data_point_string": case,
+			"field_ml_accuracy":  json.dumps(ml_model.accuracy_field),
+			"field_ml_calibration": json.dumps(ml_model.calibration_field),
+			"field_ml_prediction": model_prediction,
+			"field_ml_confidence": model_confidence,
+			"field_instance_ground_truth": ground_truth,
+			"field_user_prediction": None,
+			"field_user_did_update": None,
+			"field_user_disagree_reason_choices": None,
+			"field_user_disagree_reason_freetext": None,
+		}
 
 
-	context = {
-	    # 'feature_dict': feature_dict,
-	    'ml_confidence': model_confidence
-	}
 
-	# Render the HTML template index.html with the data in the context variable
-	return render(request, 'index.html', context=context)
+		new_user_response = ModelUserResponse(
+								field_data_point_string= initUserResponse["field_data_point_string"],
+								field_ml_accuracy = initUserResponse["field_ml_accuracy"],
+								field_ml_calibration = initUserResponse["field_ml_calibration"],
+								field_ml_prediction = initUserResponse["field_ml_prediction"],
+								field_instance_ground_truth = initUserResponse["field_instance_ground_truth"],
+								field_user_prediction = initUserResponse["field_user_prediction"],
+								field_user_did_update = initUserResponse["field_user_did_update"],
+								field_user_disagree_reason_choices = initUserResponse["field_user_disagree_reason_choices"],
+								field_user_disagree_reason_freetext = initUserResponse["field_user_disagree_reason_freetext"],
+								field_experiment = experiment
+							)
+
+		new_user_response.save()
+
+
+		context = {
+		    # 'feature_dict': feature_dict,
+		    'ml_confidence': model_confidence
+		}
+
+		# Render the HTML template index.html with the data in the context variable
+		return render(request, 'index.html', context=context)
 
 
 
