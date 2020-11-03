4124f1b9feb74920494bf09b614ff1460da46bfb
Jack <ja_killian@yahoo.com>
2020-11-03 14:08:34 -0500

    start_experiment landing page

--- human_ai_trust/mainpage/urls.py
+++ human_ai_trust/mainpage/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('start_experiment', views.start_experiment)
 ]
--- human_ai_trust/mainpage/views.py
+++ human_ai_trust/mainpage/views.py
@@ -36,5 +36,37 @@ def index(request):
 	return render(request, 'index.html', context=context)
 
 
+
+def start_experiment(request):
+	"""View function for home page of site."""
+
+	new_user_response = ModelUserResponse(field_data_point_string='0-0-0-low-low',
+							field_ml_accuracy = 0.5,
+							field_ml_confidence = 0.5,
+							field_ml_prediction = 0,
+							field_user_prediction = 1,
+							field_user_did_update = 1,
+							field_user_disagree_reason_choices = 'a',
+							field_user_disagree_reason_freetext = 'Example long text'
+							)
+
+	new_user_response.save()
+
+	# Generate counts of some of the main objects
+	all_responses = ModelUserResponse.objects.all()
+	print(len(all_responses))
+	num_responses = len(all_responses)
+
+
+	context = {
+	    'user_responses': all_responses,
+	    'num_responses': num_responses
+	}
+
+	# Render the HTML template index.html with the data in the context variable
+	return render(request, 'index.html', context=context)
+
+
+
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
