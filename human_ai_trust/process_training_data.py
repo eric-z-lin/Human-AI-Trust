d04fa32d3a21bff669bec088ab07e01ea22186d6
Kavya Kopparapu <kavyakvk@gmail.com>
2020-12-02 21:11:22 -0500

    stuff

--- human_ai_trust/process_training_data.py
+++ human_ai_trust/process_training_data.py
@@ -4,6 +4,7 @@ import cv2 as cv
 import numpy as np
 import csv
 from PIL import Image
+import random
 
 source = "train" #train,val
 #source = "val"
@@ -50,6 +51,25 @@ with open (csv_patients) as csvfile:
 			file = filename
 			file_to_diagnosis[file] = int(float(row[6]))
 #print(file_to_diagnosis)
+if(directory == "test_dir"):
+	diagnosis_pos = []
+	diagnosis_neg = []
+
+	for f in file_to_diagnosis.keys():
+		if(file_to_diagnosis[f] == 0):
+			diagnosis_neg.append(f)
+		else:
+			diagnosis_pos.append(f)
+	random.shuffle(diagnosis_pos)
+	random.shuffle(diagnosis_neg)
+
+	diagnosis_pos_0 = diagnosis_pos[0:len(diagnosis_pos)//2]
+	diagnosis_pos_1 = diagnosis_pos[len(diagnosis_pos)//2:]
+	diagnosis_neg_0 = diagnosis_neg[0:len(diagnosis_neg)//2]
+	dignosis_neg_1 = diagnosis_neg[len(diagnosis_neg)//2:]
+
+	print(len(diagnosis_pos_0), len(diagnosis_pos_1), len(diagnosis_neg_0), len(dignosis_neg_1))
+
 for filename in file_to_diagnosis.keys():
 	#filename = result[counter]
 	"""
@@ -64,30 +84,29 @@ for filename in file_to_diagnosis.keys():
 	file_arr = file.split("/")
 	name = str(file_to_diagnosis[file])+"-"+file_arr[-3]+"_"+file_arr[-2]+"_"+file_arr[-1]
 
-	#CASE 0
-	case0_image = cv.imread(file)
-	
-	#CASE 1
-	case1_image = cv.imread(file, cv.IMREAD_GRAYSCALE)
-		#overexpose
-	case1_image = cv.convertScaleAbs(case1_image, alpha=1.0, beta=10) 
-		#add noise
-	uniform_noise = np.zeros((case1_image.shape[0], case1_image.shape[1]),dtype=np.uint8)
-	cv.randu(uniform_noise,0,255)
-	uniform_noise = (uniform_noise*0.3).astype(np.uint8)
-	case1_image = cv.add(case1_image,uniform_noise)
-		#blur
-	case1_image = cv.blur(case1_image,(3,3))
-	#case1_image = image3
-	#im_pil = Image.fromarray(case1_image)
-
-
-
 	if(directory == "train_dir"):
+		case0_image = cv.imread(file)
 		cv.imwrite("mainpage/dataset/"+directory+"/"+"0-"+name, case0_image)
 	else:
-		cv.imwrite("mainpage/dataset/"+directory+"/"+"0-"+name, case0_image)
-		cv.imwrite("mainpage/dataset/"+directory+"/"+"1-"+name, case1_image)
+		if(file in diagnosis_pos_0 or file in diagnosis_neg_0):
+			case0_image = cv.imread(file)
+			cv.imwrite("mainpage/dataset/"+directory+"/"+"0-"+name, case0_image)
+		else:
+			#CASE 1
+			case1_image = cv.imread(file, cv.IMREAD_GRAYSCALE)
+				#overexpose
+			case1_image = cv.convertScaleAbs(case1_image, alpha=1.0, beta=10) 
+				#add noise
+			uniform_noise = np.zeros((case1_image.shape[0], case1_image.shape[1]),dtype=np.uint8)
+			cv.randu(uniform_noise,0,255)
+			uniform_noise = (uniform_noise*0.3).astype(np.uint8)
+			case1_image = cv.add(case1_image,uniform_noise)
+				#blur
+			case1_image = cv.blur(case1_image,(3,3))
+			#case1_image = image3
+			#im_pil = Image.fromarray(case1_image)
+
+			cv.imwrite("mainpage/dataset/"+directory+"/"+"1-"+name, case1_image)
 
 
 
